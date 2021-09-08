bd4693378eb45afa06e3a77467819502dce23d36
AgentEnder <craigorycoppola@gmail.com>
2021-09-08 16:17:36 -0500

    Add nx-dotnet supporting plugin

---
+++ apps/shop/mvc-test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MultilanguagePlayground.ShopMvc.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ apps/shop/mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MultilanguagePlayground.ShopMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
