1a221f2cfd50d786300d4c0635830bf647b86183
AgentEnder <craigorycoppola@gmail.com>
2021-09-08 16:47:12 -0500

    chore: scaffold ng app

---
+++ apps/modern-shop/app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'multilanguage-playground-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'modern-shop';
+}
